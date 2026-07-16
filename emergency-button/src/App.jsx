efb9d0a4c6666cb14d4422301ae54016ace4b767
junibbby <junibbby@example.com>
2026-07-16 15:48:49 +0700

    Button Implemented

--- emergency-button/src/App.jsx
+++ emergency-button/src/App.jsx
@@ -10,16 +10,14 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+        <div>
+          <h1>EMERGENCY BUTTON</h1>
+          <p>
+            someone put the carousel here!!!!!!!!!!!
+          </p>
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+          <a href="tel:+62112" class="sosbutton">CALL</a>
         </div>
         <button
           type="button"
